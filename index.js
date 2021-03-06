5472ecbac3536c0ee853a8811b82524d954ee155
Jarod Christiansen <jarodchristiansen2@gmail.com>
2021-03-06 11:53:22 -0800

    survey additions

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
 
@@ -28,8 +29,9 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutres')(app);
+require('./routes/surveyRoutes')(app);
 
-if(!process.env.NODE_ENV === 'production') {
+if(process.env.NODE_ENV === 'production') {
     //Express will serve up production assets like our main.js file, or main css file
 
     app.use(express.static('client/build'));
